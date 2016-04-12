1899c1df7609a263a0c7c33f931253c5f4aa00d8
kaz-chan <kooljapanese@gmail.com>
2016-04-11 22:02:04 -0400

    Minor changes

--- AspAssignment/CustomersAdmin.aspx.cs
+++ AspAssignment/CustomersAdmin.aspx.cs
@@ -11,4 +11,52 @@ public partial class _Default : System.Web.UI.Page
     {
 
     }
+    protected void DetailsViewCustomerDetails_ItemDeleted(object sender, DetailsViewDeletedEventArgs e)
+    {
+        if (e.Exception != null)
+        {
+            lblErrorMessage.Text = "A database error has occured. "
+                + "Message: " + e.Exception.Message;
+            e.ExceptionHandled = true;
+        }
+        else if (e.AffectedRows == 0)
+        {
+            lblErrorMessage.Text = "Another user may have updated that product. "
+                + "Please try again.";
+        }
+        else
+            GridViewCustomerList.DataBind();
+    }
+    protected void DetailsViewCustomerDetails_ItemUpdated(object sender, DetailsViewUpdatedEventArgs e)
+    {
+        if (e.Exception != null)
+        {
+            lblErrorMessage.Text = "A database error has occured. "
+                + "Message: " + e.Exception.Message;
+            e.ExceptionHandled = true;
+        }
+        else if (e.AffectedRows == 0)
+        {
+            lblErrorMessage.Text = "Another user may have updated that product. "
+                + "Please try again.";
+        }
+        else
+            GridViewCustomerList.DataBind();
+    }
+    protected void DetailsViewCustomerDetails_ItemInserted(object sender, DetailsViewInsertedEventArgs e)
+    {
+        if (e.Exception != null)
+        {
+            lblErrorMessage.Text = "A database error has occured. "
+                + "Message: " + e.Exception.Message;
+            e.ExceptionHandled = true;
+        }
+        else if (e.AffectedRows == 0)
+        {
+            lblErrorMessage.Text = "Another user may have updated that product. "
+                + "Please try again.";
+        }
+        else
+            GridViewCustomerList.DataBind();
+    }
 }
